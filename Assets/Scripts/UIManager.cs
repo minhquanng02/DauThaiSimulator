bd4303bc7d96bcd666d6d6b13fff295c5a9b097b
QuanNguyen <quan0986066955@gmail.com>
2026-03-03 13:51:12 +0700

    done start scene load scene

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -6,27 +6,44 @@ using static UnityEngine.Rendering.DebugUI;
 
 public class UIManager : MonoBehaviour
 {
-    [SerializeField] private GameObject loadScene;
-    [SerializeField] private GameObject startScene;
+  
 
     [SerializeField] private RectTransform panel;
     [SerializeField] private RectTransform header;
+    [SerializeField] private RectTransform pauseBtn;
 
-    [SerializeField] private float panelPosY = 70f;
-    [SerializeField] private float headerPosY = -33f;
+    [SerializeField] private float panelStart = -80f;
+    [SerializeField] private float headetStart = 40f;
+    [SerializeField] private float pauseBtnStart = 35f;
 
-    [SerializeField] private float panelDuration = 1f;
+    [SerializeField] private float duration = 1f;
 
-    public void OnPressStart()
+    [SerializeField] private float panelPosY; 
+    [SerializeField] private float headerPosY;
+    [SerializeField] private float pauseBtnPosX;
+
+    private void Awake()
     {
-        startScene.SetActive(false);
+       
+    }
+
+    private void Start()
+    {
+         panelPosY = panel.anchoredPosition.y;
+         headerPosY = header.anchoredPosition.y;
+        pauseBtnPosX = pauseBtn.anchoredPosition.x;
+
+        panel.anchoredPosition = new Vector2(panel.anchoredPosition.x, panelStart);
+        header.anchoredPosition = new Vector2(header.anchoredPosition.x, headetStart);
+        pauseBtn.anchoredPosition = new Vector2(pauseBtnStart, pauseBtn.anchoredPosition.y);
+
     }
 
     public void OnPressPlay()
     {
-        loadScene.SetActive(false);
 
-        panel.DOAnchorPosY(panelPosY, panelDuration).SetEase(Ease.InOutCubic);
-        header.DOAnchorPosY(headerPosY, panelDuration).SetEase(Ease.InOutCubic);
+        panel.DOAnchorPosY(panelPosY, duration).SetEase(Ease.InOutCubic);
+        header.DOAnchorPosY(headerPosY, duration).SetEase(Ease.InOutCubic);
+        pauseBtn.DOAnchorPosX(pauseBtnPosX, duration).SetEase(Ease.InOutCubic);
     }
 }
